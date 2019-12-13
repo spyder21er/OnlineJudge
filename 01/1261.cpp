eceea5355d4e912efb6a65abc47d347239d2b8ac
Allen Mabana <spyder21er@gmail.com>
2019-12-13 09:51:59 +0800

    Update 1261.cpp

--- 01/1261.cpp
+++ 01/1261.cpp
@@ -3,16 +3,24 @@
  * Author: leaf.mabz
  */
 
+/**
+ * Online Judge 1261 - String Popping
+ * Author: leaf.mabz
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 
-typedef map<int, bool>::iterator mipIter;
-map<int, bool> poptable;
+typedef map<size_t, bool> mib;
+typedef mib::iterator mibIter;
+
+mib poptable;
+vector<mib> DP(26, poptable);
 
 int can_be_popped(int n)
 {
-    mipIter it = poptable.find(n);
+    mibIter it = poptable.find(n);
     if (it == poptable.end()) // not found
         return -1;
     
@@ -65,41 +73,98 @@ bool pop(int str, int size)
     // has one group and poppable
     if (size == 1 && str == 1) return true;
 
-    bitset<size> bs (str);
+    // has one group and not poppable
+    if (size == 1 && str == 0) return false;
+
+    bitset<25> bs (str);
 
     // has only one poppable at the edge
-    if (bs.count() == 1 && (str == 1 || str == (1 << (size - 1)))) return false;
+    if ((int) bs.count() == 1 && (str == 1 || str == (1 << (size - 1)))) return false;
 
     // odd number of groups and bit at the very middle is set
-    if (bs.test(size >> 1)) return true;
+    if ((size & 1) && (bs.test(size >> 1))) return true;
 
     // all groups are poppable
-    if (bs.count() == size) return true;
+    if ((int) bs.count() == size) return true;
 
+    // let's check if it is already solved and return the solution
+    hash< bitset<25> > hasher;
+    size_t key = hasher(bs); // we will use hash of the bitset as key
+    if (DP[size].find(key) != DP[size].end()) return DP[size][key];
+
+    // let's also check the reverse
+    bitset<25> bscopy = bs;
+    for (int i = 0; i < (size >> 1); i++)
+    {
+        bool temp = bscopy[i];
+        bscopy[i] = bscopy[size - i - 1];
+        bscopy[size - i - 1] = temp;
+    }
+    key = hasher(bscopy);
+    if (DP[size].find(key) != DP[size].end()) return DP[size][key];
+
+    // else we will solve it manually
+    bool result = false;
     for (int i = 0; i < size; i++)
     {
         if (bs.test(i))
         {
-            // todo:
-            return false;
+            int new_str = str;
+
+            // if it is at the edge
+            if (i == 0 || i == (size - 1)) {
+                // pop that edge and solve
+                if (i == 0)
+                    new_str >>= 1;
+                else
+                    new_str -= (1 << (size-1));
+
+                if (pop(new_str, size - 1)) 
+                {
+                    result = true;
+                    break;
+                }
+            } else { // at the middle
+                // we remove the bits left and right of i
+                new_str = (str >> (i + 2));
+                new_str <<= 1;
+                new_str += 1;
+                new_str <<= (i-1);
+                int right_part = 0;
+                for (int j = 0; j < (i-1); j++)
+                {
+                    right_part <<= 1;
+                    right_part += 1;
+                }
+                new_str += (right_part & str);
+
+                // since we remove left and right bits, our size is now size - 2
+                if (pop(new_str, size - 2)) 
+                {
+                    result = true;
+                    break;
+                }
+            }
         }
     }
+
+    DP[size][key] = result;
+    return result;
 }
 
 int main()
 {
     string s;
-    pair<int, int> ret;
-    // let's test
-    while (true)
+    pair<int, int> parsed;
+    int t;
+
+    scanf("%d", &t);
+    while (t--)
     {
         cin >> s;
-        if (s == "END") break;
-        ret = parse(s);
-        int size = ret.second, str = ret.first;
-        bitset<16> bs (ret.first);
-        cout << bs << "|" << ret.second << endl;
-        cout << ((size & 1) && ((1 << (size >> 1)) & str)) << endl;
+        parsed = parse(s);
+        int size = parsed.second, str = parsed.first;
+        cout << (pop(str, size) ? 1 : 0) << endl;
     }
     return 0;
 }
