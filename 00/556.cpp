f17fc58d32682e2d4c87fa96c039a56ed5ca3de9
spyder21er <spyder21er@gmail.com>
2020-01-06 02:09:54 +0800

    Update 556

--- 00/556.cpp
+++ 00/556.cpp
@@ -6,22 +6,28 @@ int frontr[4] = {-1, 0, 1, 0};
 int frontc[4] = {0, 1, 0, -1};
 
 // visit the cell at loc
-void visit(vector< int > &v, vector< int > &mz, const pair< int, int > &loc, const int &c) {
+void moveForward(vector< int > &v, vector< int > &mz, pair< int, int > &loc, const int &c, const int &face) {
+    // move loc to cell forward
+    loc.first  += frontr[face];
+    loc.second += frontc[face];
+    // get the number of times we visited this cell
     int i = mz[loc.first * c + loc.second];
-    v[i]--;
-    v[i + 1]++;
+    v[i]--; // the cell we visited i times will now 
+    v[i + 1]++; // be visited i + 1 times
     mz[loc.first * c + loc.second]++;
 }
 
-bool canMoveForward(const vector< int > mz, const pair< int, int > &loc, const int &face, const int &b, const int &w) {
+bool canMoveForward(const vector< int > &mz, const pair< int, int > &loc, const int &face, const int &b, const int &w) {
+    // let (row, col) be a cell in our front where we are facing
     int row = loc.first + frontr[face];
     int col = loc.second + frontc[face];
+    // check if we are not hitting a wall
     bool inbound = row >= 0 && row < b && col >= 0 && col < w;
+    // return true if we are inbound and not in a close square, false otherwise
     return (inbound) ? (mz[row * w + col] != -1) : false;
 }
 
-int main()
-{
+int main() {
     int b, w, start, r, c, face;
     char cell;
     vector< int > visits, maze;
@@ -55,35 +61,26 @@ int main()
         face = 1; loc = make_pair(start, 0);
 
         // we need to leave start cell before travelling the maze
-        // while we can't move forward, turnleft
+        // while we can't move forward, turn left
         while (!canMoveForward(maze, loc, face, b, w)) {
             face = (face + 3) % 4;
         }
-        // move forward
-        loc.first  += frontr[face];
-        loc.second += frontc[face];
-        // visit this cell
-        visit(visits, maze, loc, w);
+        moveForward(visits, maze, loc, w, face);
         
         // now let's travel around the maze while we are not at the starting point
         while (loc.first != start || loc.second != 0) {
-            // turnright
-            face = (face + 1) % 4;
+            face = (face + 1) % 4; // turn right
             // while we can't move forward, turnleft
             while (!canMoveForward(maze, loc, face, b, w)) {
                 face = (face + 3) % 4;
             }
-            // move forward
-            loc.first += frontr[face];
-            loc.second += frontc[face];
-            // visit this cell
-            visit(visits, maze, loc, w);
+            moveForward(visits, maze, loc, w, face);
         }
 
         for (int i = 0; i < 5; i++)
             printf("%3d", visits[i]);
 
-        cout << endl;
+        printf("\n");
     }
 
     return 0;
