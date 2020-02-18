21a720294f27b5781979c4ec5c8fc8f4d0b731f1
spyder21er <spyder21er@gmail.com>
2020-02-18 21:50:24 +0800

    Improved 12398

--- 12/12398.cpp
+++ 12/12398.cpp
@@ -1,42 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int total(int * M, string indices) {
+    int sum = 0;
+    for (auto c : indices) {
+        sum += M[(int) (c - '0')];
+    }
+    return sum % 10;
+}
 
 int main()
 {
     string moves;
-    int casenum = 0;
-    int board[3][3];
+    int casenum = 0, scores[9], a, b, c, d, e, f, g, h, i;
 
     while (getline(cin, moves)) {
-        memset(board, 0, sizeof(int) * 9);
+        memset(scores, 0, sizeof(scores));
+        
         for (int i = moves.length() - 1; i >= 0; i--) {
             int index = (int) (moves[i] - 'a');
-
-            // itself
-            board[index / 3][index % 3] = (board[index / 3][index % 3] + 1) % 10;
-            // up
-            if ((index / 3) > 0)
-                board[(index / 3) - 1][index % 3] = (board[(index / 3) - 1][index % 3] + 1) % 10;
-            // down
-            if ((index / 3) < 2)
-                board[(index / 3) + 1][index % 3] = (board[(index / 3) + 1][index % 3] + 1) % 10;
-            // left
-            if ((index % 3) > 0)
-                board[(index / 3)][(index % 3) - 1] = (board[(index / 3)][(index % 3) - 1] + 1) % 10;
-            // right
-            if ((index % 3) < 2)
-                board[(index / 3)][(index % 3) + 1] = (board[(index / 3)][(index % 3) + 1] + 1) % 10;
+            scores[index]++;
         }
 
+        a = total(scores, "013");
+        b = total(scores, "0124");
+        c = total(scores, "125");
+        d = total(scores, "0346");
+        e = total(scores, "13457");
+        f = total(scores, "2458");
+        g = total(scores, "367");
+        h = total(scores, "4678");
+        i = total(scores, "578");
         printf("Case #%d:\n", ++casenum);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                printf("%d", board[i][j]);
-                if (j < 2) printf(" "); else printf("\n");
-            }
-        }
+        printf("%d %d %d\n%d %d %d\n%d %d %d\n", a, b, c, d, e, f, g, h, i);
     }
 
     return 0;
