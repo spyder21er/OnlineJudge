6ab62d4814554f03937427f6ce63a0c26ae0e1d0
spyder21er <spyder21er@gmail.com>
2020-02-29 00:13:45 +0800

    Updated 278

--- 00/278.cpp
+++ 00/278.cpp
@@ -10,28 +10,16 @@ int main() {
     while (T--) {
         scanf(" %c", &c);
         scanf("%d %d", &m, &n);
-
         if (c == 'r' || c == 'Q') {
-            printf("%d\n", min(m, n));
+            p= min(m, n);
         }
-
         if (c == 'k') {
-            printf("%d\n", ((m * n) + 1) / 2);
+            p = ((m * n) + 1) / 2;
         }
-
         if (c == 'K') {
-            // both are even
-            if (!(m & 1) && !(n & 1))
-                p = (m*n) / 4;
-            else if (m & 1 && n & 1) { // both are odd
-                p = ((m - 1) * (n - 1) / 4) + (n / 2) + (m / 2) + 1;
-            } else if (n & 1) { // n is odd
-                p = (m * (n - 1) / 4) + (n / 2) + 1;
-            } else if (n & 1) { // n is odd
-                p = ((m - 1) * n / 4) + (m / 2) + 1;
-            }
-            printf("%d\n", p);
+            p = ((m + 1) / 2) * ((n + 1) / 2);
         }
+        printf("%d\n", p);
     }
     return 0;
 }
