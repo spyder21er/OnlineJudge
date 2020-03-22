6837ec6fb3e5ecb546bc71d7ce611a99701cb795
spyder21er <spyder21er@gmail.com>
2020-03-22 18:39:19 +0800

    Added 696

--- 00/696.cpp
+++ 00/696.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int m, n;
+int main() {
+    int m, n, k, max_mn;
 
-    while (true)
-    {
-        cin >> m >> n;
+    while (true)     {
+        scanf("%d %d", &m, &n);
 
-        if ((m + n) == 0)
-            break;
+        if (!(m + n)) break;
+ 
+        max_mn = max(m, n);
 
-        int k;
+        if (!m || !n)
+            k = 0;
+        else if (m == 1 || n == 1)
+            k = max_mn;
+        else if (m == 2 || n == 2) {
+            k = 4 * (max_mn / 4) +  2 * min((max_mn % 4), 2);
+        } else k = ((m * n) + 1) / 2;
 
-        
+        printf("%d knights may be placed on a %d row %d column board.\n", k, m, n);
     }
 
     return 0;
