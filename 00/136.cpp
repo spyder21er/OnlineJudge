8523993f7cd1c1ca3018d178cfd63b0e649cb7c2
spyder21er <spyder21er@gmail.com>
2021-01-04 21:27:39 +0800

    Add 136

---
+++ 00/136.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // vector<long long> uglies;
+
+    // uglies.push_back(1);
+
+    // for (long long i = 2; uglies.size() < 1500; i++) {
+    //     long long n = i;
+    //     while (!(n & 1)) {
+    //         n >>= 1;
+    //     }
+    //     while (n % 3 == 0)
+    //     {
+    //         n /= 3;
+    //     }
+    //     while (n % 5 == 0) {
+    //         n /= 5;
+    //     }
+    //     if (n == 1) {
+    //         uglies.push_back(i);
+    //     }
+    // }
+    // printf("%lld\n", uglies[1499]);
+    printf("The 1500'th ugly number is 859963392.\n");
+    return 0;
+}
