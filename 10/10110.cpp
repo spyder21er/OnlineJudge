d1d373e1decdff59c682dfbe68d167bf46521709
spyder21er <spyder21er@gmail.com>
2021-01-05 21:10:21 +0800

    Add 10110

---
+++ 10/10110.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unsigned int n, root;
+
+    while (scanf("%d", &n), n) {
+        root = sqrt(n);
+        if (root * root == n) {
+            printf("yes\n");
+        } else {
+            printf("no\n");
+        }
+    }
+    return 0;
+}
