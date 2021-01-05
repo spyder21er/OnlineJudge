7e514cbd1d989890841ef1e1a7d47d0e34789abf
spyder21er <spyder21er@gmail.com>
2021-01-05 08:44:45 +0800

    Add 113

---
+++ 00/113.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double n, p;
+
+    while (scanf("%lf %lf", &n, &p) != EOF) {
+        printf("%.0lf\n", pow(p, 1.0/n));
+    }
+    return 0;
+}
